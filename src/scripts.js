71f7fcd552d11029ea0ec51fe5fc2817a3b317d0
MoyArt <moyart@gmail.com>
2024-07-20 19:18:43 -0500

    feat(player): adding player 1 sprite and debug mode

--- src/scripts.js
+++ src/scripts.js
@@ -16,6 +16,8 @@ window.addEventListener('load', function () {
           this.game.keys.push(e.key);
         } else if (e.key === ' ') {
           this.game.player.shootTop();
+        } else if (e.key === 'd') {
+          this.game.debug = !this.game.debug;
         }
         console.log(this.game.keys);
       });
@@ -55,9 +57,13 @@ window.addEventListener('load', function () {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
       this.speedY = 0;
       this.maxSpeed = 5;
       this.projectiles = [];
+      this.image = document.getElementById('player');
     }
     update() {
       if (this.game.keys.includes('ArrowUp')) {
@@ -75,10 +81,28 @@ window.addEventListener('load', function () {
       this.projectiles = this.projectiles.filter(
         (projectile) => !projectile.markedForDeletion
       );
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
     }
     draw(context) {
-      context.fillStyle = 'pink';
-      context.fillRect(this.x, this.y, this.width, this.height);
+      if (this.game.debug) {
+        context.strokeRect(this.x, this.y, this.width, this.height);
+      }
+
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       this.projectiles.forEach((projectile) => {
         projectile.draw(context);
       });
@@ -240,6 +264,7 @@ window.addEventListener('load', function () {
       this.gameTime = 0;
       this.timeLimit = 5000;
       this.speed = 1;
+      this.debug = true;
     }
     update(deltaTime) {
       if (!this.gameOver) {
