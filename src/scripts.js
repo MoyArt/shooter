2feb11e9dc08f95c19548ca58dd59fd33396eb2c
MoyArt <moyart@gmail.com>
2024-07-22 00:25:31 -0500

    feat(particles): added

--- src/scripts.js
+++ src/scripts.js
@@ -52,6 +52,14 @@ window.addEventListener('load', function () {
     }
   }
 
+  class Particle {
+    constructor(game, x, y) {
+      this.game = game;
+      this.x = x;
+      this.y = y;
+    }
+  }
+
   class Player {
     constructor(game) {
       this.game = game;
